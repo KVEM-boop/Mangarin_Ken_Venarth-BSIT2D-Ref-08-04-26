ab8519f11aa143691fd7a1ca3020fccc39a547e9
KVEM-boop <mangarinkenvenarth@gmail.com>
2026-08-12 17:47:19 +0800

    Add files via upload

---
+++ Mangarin_Ken_Venarth_E Ref 08_12_26.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class HelloWorld
+{
+    static public void Main()
+    {
+        Console.WriteLine("Enter Your Name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter Your Age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter your weight: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        bool fls = true;
+        Console.WriteLine("Hello, " + name + " You are " + age + " Years Old! And Weight " + weight.ToString("F2") + "kg and this is " + fls + ".");
+    }
+}
